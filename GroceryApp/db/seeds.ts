eb87d910f9895664f4b051a5594937150d668a6c
Patrick Engelkes <patrick.engelkes@finstreet.de>
2021-02-20 20:56:49 +0100

    Add seeds.

--- GroceryApp/db/seeds.ts
+++ GroceryApp/db/seeds.ts
@@ -1,16 +1,36 @@
-// import db from "./index"
+import db from "./index"
+import faker from "faker"
+import { SecurePassword } from "blitz"
 
 /*
  * This seed function is executed when you run `blitz db seed`.
- *
- * Probably you want to use a library like https://chancejs.com
- * or https://github.com/Marak/Faker.js to easily generate
- * realistic data.
  */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  const hashedPassword = await SecurePassword.hash("my-secret-password")
+
+  const user = await db.user.create({
+    data: {
+      email: faker.internet.email(),
+      hashedPassword,
+      role: "USER",
+    },
+  })
+
+  const list = await db.list.create({
+    data: {
+      name: faker.lorem.word(10),
+      userId: user.id,
+    },
+  })
+
+  for (let i = 0; i < 5; i++) {
+    await db.item.create({
+      data: {
+        name: faker.lorem.word(8),
+        listId: list.id,
+      },
+    })
+  }
 }
 
 export default seed
