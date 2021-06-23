f4e257a4f0fb6973fac21810c5da5f4a5a9cf895
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-23 18:01:42 +0800

    Trigger email to send on signup mutation

--- GroceryApp/app/auth/mutations/signup.ts
+++ GroceryApp/app/auth/mutations/signup.ts
@@ -1,7 +1,10 @@
-import { resolver, SecurePassword } from "blitz"
+import { resolver, SecurePassword, generateToken, hash256 } from "blitz"
 import db from "db"
 import { Signup } from "app/auth/validations"
 import { Role } from "types"
+import { emailVerificationMailer } from "mailers/emailVerificationMailer"
+
+const EMAIL_VERIFICATION_TOKEN_EXPIRATION_IN_HOURS = 4
 
 export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
   const hashedPassword = await SecurePassword.hash(password.trim())
@@ -11,5 +14,22 @@ export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, c
   })
 
   await ctx.session.$create({ userId: user.id, role: user.role as Role })
+
+  // Set up email verification email
+  const token = generateToken()
+  const hashedToken = hash256(token)
+  const expiresAt = new Date()
+  expiresAt.setHours(expiresAt.getHours() + EMAIL_VERIFICATION_TOKEN_EXPIRATION_IN_HOURS)
+
+  await db.token.create({
+    data: {
+      user: { connect: { id: user.id } },
+      type: "VERIFY_EMAIL",
+      expiresAt,
+      hashedToken,
+      sentTo: email.toLowerCase().trim(),
+    },
+  })
+  await emailVerificationMailer({ to: email.toLowerCase().trim(), token }).send()
   return user
 })
