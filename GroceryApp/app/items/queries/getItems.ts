ca3f82f17eaa041c8e2e620c3fc9b7f38e578d5d
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-06-23 17:01:37 +0800

    Add generated code from 38.1 upgrade

--- GroceryApp/app/items/queries/getItems.ts
+++ GroceryApp/app/items/queries/getItems.ts
@@ -8,7 +8,12 @@ export default resolver.pipe(
   resolver.authorize(),
   async ({ where, orderBy, skip = 0, take = 100 }: GetItemsInput) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const { items: items, hasMore, nextPage, count } = await paginate({
+    const {
+      items: items,
+      hasMore,
+      nextPage,
+      count,
+    } = await paginate({
       skip,
       take,
       count: () => db.item.count({ where }),
